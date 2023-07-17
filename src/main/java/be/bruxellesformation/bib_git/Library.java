0619e9b1f5d31da1feafe3b2840eacaecfe1c879
bram-pc <bram.labarque@gmail.com>
2023-07-17 17:44:40 +0200

    todo method holders in library (test from home)

--- src/main/java/be/bruxellesformation/bib_git/Library.java
+++ src/main/java/be/bruxellesformation/bib_git/Library.java
@@ -3,19 +3,33 @@ package be.bruxellesformation.bib_git;
 public class Library {
 
     // Ajouter un livre dans la bibliothèque
-    public void addBook(){}
+    public void addBook(){
+        //todo addBook
+    }
 
     // Supprimer un livre
-    public void deleteBook(int bookId){}
-
-    //public Book searchBook(String )
+    public void deleteBook(int bookId){
+        //todo deleteBook
+    }
 
     // Chercher un livre (par auteur et nom en même temps)
+    public Book searchBook(String author, String title){
+        // todo searchBook
+    }
 
     // Afficher tous les livres
+    public void showBooks(){
+        //todo showBooks
+    }
 
     // Emprunter un livre
+    public void lendBook(int bookId){
+        //todo lendBook
+    }
 
     // Rendre un livre
+    public void returnBook(int bookId){
+        //todo returnBook
+    }
 
 }
