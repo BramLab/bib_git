c94d4934085399dffc89cb278e5278e75dfc4f94
usil89 <139747259+usil89@users.noreply.github.com>
2023-07-17 15:17:30 +0200

    main method menu

--- src/main/java/be/bruxellesformation/bib_git/Main.java
+++ src/main/java/be/bruxellesformation/bib_git/Main.java
@@ -2,7 +2,12 @@ package be.bruxellesformation.bib_git;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("+------------------------------+");
+        System.out.println("Welcome to our Book Application.");
+        System.out.println("+------------------------------+");
+
+        Book book1 = new Book(1,"Harry Potter and the Cursed Child","J.K. Rowling");
+        Book book2 = new Book(2,"The Christie Affair","Nina de Gramont");
     }
 
 }
