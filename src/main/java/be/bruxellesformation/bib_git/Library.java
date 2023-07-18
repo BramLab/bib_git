e8e184294bd4bdcd110512d11c1a7e39e80ab285
usil89 <139747259+usil89@users.noreply.github.com>
2023-07-18 13:08:21 +0200

    main ajout library methods

--- src/main/java/be/bruxellesformation/bib_git/Library.java
+++ src/main/java/be/bruxellesformation/bib_git/Library.java
@@ -1,21 +1,88 @@
 package be.bruxellesformation.bib_git;
 
+import java.util.*;
+
+import be.bruxellesformation.bib_git.Book;
+
 public class Library {
 
-    // Ajouter un livre dans la bibliothèque
-    public void addBook(){}
+    List<Book> books;
 
-    // Supprimer un livre
-    public void deleteBook(int bookId){}
+    public Library() {
+        books = new ArrayList<Book>();
 
-    //public Book searchBook(String )
+        books.add(new Book(1, "Harry Potter and the Cursed Child", "J.K. Rowling"));
+        books.add(new Book(2, "The Christie Affair", "Nina de Gramont"));
+    }
 
-    // Chercher un livre (par auteur et nom en même temps)
+    public void addBook(String bookName, String autName){
+        Book bookWithHighestBookIdv1 = books.stream().max(Comparator.comparing(v -> v.getBookId())).get(); // googled
+        //Book bookWithHighestBookIdv2 = books.stream().max(Comparator.comparing(Book::getBookId)).get();    // googled
+        books.add(new Book(bookWithHighestBookIdv1.getBookId()+1, bookName, autName));
+    }
 
-    // Afficher tous les livres
+    public void updateBook(int bookId, String bookName, String autName){
+        ListIterator<Book> bookListIterator  = books.listIterator();
+        while (bookListIterator.hasNext()) {
+            Book book = bookListIterator.next();
+            if (book.getBookId() == bookId){
+                bookListIterator.set(new Book(bookId, bookName, autName));
+            }
+        }
+    }
 
-    // Emprunter un livre
+    public boolean deleteBook(int bookId){
+        //todo deleteBook
+        boolean found = false;
+        Iterator<Book> itBook = books.iterator();
+        itBook  = books.iterator();
+        while (itBook.hasNext()) {
+            Book book = itBook.next();
+            if (book.getBookId() == bookId){
+                itBook.remove();
+                found= true;
+            }
+        }
+        return found;
+    }
 
-    // Rendre un livre
+    /* Chercher un livre (par auteur et nom en même temps)
+    public Book searchBook(String author, String title){
+        return null;// todo searchBook
+    }*/
+
+    public boolean searchBook(int bookId) {
+        Iterator<Book> itBook = books.iterator();
+        boolean found = false;
+
+        while (itBook.hasNext()) {
+            Book book = itBook.next();
+            if (book.getBookId() == bookId) {
+                System.out.println(book);
+                found = true;
+            }
+        }
+        return found;
+    }
+
+    public void showBooks(){
+        //todo showBooks
+        System.out.println("+------------------------------------------------------------+");
+        Iterator<Book> itBook = books.iterator();
+        while (itBook.hasNext()) {
+            Book book = itBook.next();
+            System.out.println(book);
+        }
+        System.out.println("+------------------------------------------------------------+");
+    }
+
+
+    public void lendBook(int bookId){
+        //todo lendBook
+    }
+
+    public void returnBook(int bookId){
+        //todo returnBook
+    }
 
 }
